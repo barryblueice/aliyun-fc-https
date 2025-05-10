34705d6e5824a57288edbc1befd0f6f6da0aed9a
Barryblueice <barryblueice@outlook.com>
2025-05-10 13:14:57 +0800

    添加certbot输出

--- api/certbot.py
+++ api/certbot.py
@@ -1,23 +1,39 @@
 import subprocess
-import logging
 import time
-
-logger = logging.getLogger(__name__)
+from loguru import logger
 
 def certbot_update(domain: str):
     command = f"certbot certonly --force-renewal --manual -d '*.{domain}' --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory --key-type rsa"
+    
+    # command = f"screenfetch"
 
     time.sleep(1)
 
     try:
-        subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
-    except:
-        pass
+        result = subprocess.run(
+            command,
+            shell=True,
+            check=True,
+            text=True,
+            capture_output=True,
+            encoding="utf-8",
+        )
 
-    time.sleep(1)
+        logger.info("Certbot Updating process finished！Output：\n{}", result.stdout)
 
-    logger.info("Update SSL Complete")
+    except subprocess.CalledProcessError as e:
+        logger.error(
+            "Certbot Updating process fail, Error Code: {}\nError Output:\n{}\nStandard Output:\n{}",
+            e.returncode,
+            e.stderr,
+            e.stdout,
+        )
+        raise 
 
-    time.sleep(1)
+    except Exception as e:
+        logger.error("Unknown Error: {}", e)
+        raise
 
-    return
+    else:
+        logger.info("Update SSL Complete!")
+        return
